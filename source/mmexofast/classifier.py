579972cadb7b526a1956a0258f18d0efa4acc3bd
mj.mroz <mj.mroz@uw.edu.pl>
2026-08-25 17:19:31 +0200

    6)

--- source/mmexofast/classifier.py
+++ source/mmexofast/classifier.py
@@ -98,20 +98,28 @@ class AnomalyClassifier(object):
             self._template_fit1 = BellTemplateFitter(
                 residuals, lc_parameters, n_bells=1
             )
-            self._template_fit1.run()
-            print(
-                f"1-bell template fit results: {self._template_fit1.best['chi2']}"
-            )
-            # print(f"best: {self._template_fit1.best}")
+            try:
+                self._template_fit1.run()
+                print(
+                    f"1-bell template fit results: {self._template_fit1.best['chi2']}"
+                )
+                # print(f"best: {self._template_fit1.best}")
+            except Exception as e:
+                print(f"1-bell template fit failed: {e}")
+                self._template_fit1.best = {"chi2": np.inf}
 
             self._template_fit2 = BellTemplateFitter(
                 self.residuals, self.lc_parameters, n_bells=2
             )
-            self._template_fit2.run()
-            print(
-                f"2-bell template fit results: {self._template_fit2.best['chi2']}"
-            )
-            # print(f"best: {self._template_fit2.best}")
+            try:
+                self._template_fit2.run()
+                print(
+                    f"2-bell template fit results: {self._template_fit2.best['chi2']}"
+                )
+                # print(f"best: {self._template_fit2.best}")
+            except Exception as e:
+                print(f"2-bell template fit failed: {e}")
+                self._template_fit2.best = {"chi2": np.inf}
 
         if np.abs(lc_parameters["u_0"]) < 0.01:
             return "high_mag"
@@ -120,7 +128,7 @@ class AnomalyClassifier(object):
             if np.abs(lc_parameters["u_0"]) > 0.05:
                 if (
                     self._template_fit1.best["chi2"]
-                    < self._template_fit2.best["chi2"]
+                    <= self._template_fit2.best["chi2"]
                 ):
                     return "bump"
                 else:
